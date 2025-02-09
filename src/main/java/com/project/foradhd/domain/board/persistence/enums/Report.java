12a25149bc397810e67d7ad161dd4bb817290162
kingjinyong <wlsdydwhswkf@gmail.com>
2025-02-09 21:33:28 +0900

    feat: 신고기능 신고 유형 Enum 작성

---
+++ src/main/java/com/project/foradhd/domain/board/persistence/enums/Report.java
@@ -0,0 +1,9 @@
+package com.project.foradhd.domain.board.persistence.enums;
+
+public enum Report {
+    INSULT_AND_DEGRADE,
+    MISINFORMATION,
+    PERSONAL_INFO_LEAK,
+    COMMERCIAL_ADVERTISEMENT,
+    HATEFUL_CONTENT
+}
