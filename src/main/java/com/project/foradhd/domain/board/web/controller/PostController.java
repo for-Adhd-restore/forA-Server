e51b22254bf6259af3fc8a6371524edc4fa5ce08
kingjinyong <wlsdydwhswkf@gmail.com>
2025-03-16 21:34:19 +0900

    fix: 신고처리 기능 DTO 매핑처리

--- src/main/java/com/project/foradhd/domain/board/web/controller/PostController.java
+++ src/main/java/com/project/foradhd/domain/board/web/controller/PostController.java
@@ -10,6 +10,7 @@ import com.project.foradhd.domain.board.persistence.enums.Category;
 import com.project.foradhd.domain.board.persistence.enums.HandleReport;
 import com.project.foradhd.domain.board.persistence.enums.Report;
 import com.project.foradhd.domain.board.persistence.enums.SortOption;
+import com.project.foradhd.domain.board.web.dto.request.HandleReportRequest;
 import com.project.foradhd.domain.board.web.dto.request.PostRequestDto;
 import com.project.foradhd.domain.board.web.dto.request.ReportTypeDto;
 import com.project.foradhd.domain.board.web.dto.response.PostListResponseDto;
@@ -362,10 +363,13 @@ public class PostController {
 
     // 신고 처리 API
     @PostMapping("/handleReport")
-    public ResponseEntity<Void> handleReport(@RequestBody String email,
-                                             @RequestBody Long postId,
-                                             @RequestBody HandleReport handleReportType){
-        postReportService.handleReport(email, postId, handleReportType);
+    public ResponseEntity<Void> handleReport(@RequestBody HandleReportRequest handleReportRequest){
+        postReportService.handleReport(
+                handleReportRequest.getEmail(),
+                handleReportRequest.getPostId(),
+                handleReportRequest.getHandleReportType()
+        );
+
         return ResponseEntity.ok().build();
     }
 }
