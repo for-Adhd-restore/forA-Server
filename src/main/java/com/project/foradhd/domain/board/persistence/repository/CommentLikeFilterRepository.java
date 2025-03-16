d9c07e2b65e26dc64203ba63f2aea835b2a64c54
newoceanwave <leeseohyun02@naver.com>
2025-03-16 21:24:08 +0900

    fix: 게시글 댓글 조회시 500에러 수정

--- src/main/java/com/project/foradhd/domain/board/persistence/repository/CommentLikeFilterRepository.java
+++ src/main/java/com/project/foradhd/domain/board/persistence/repository/CommentLikeFilterRepository.java
@@ -4,6 +4,7 @@ import com.project.foradhd.domain.board.persistence.entity.CommentLikeFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -22,4 +23,8 @@ public interface CommentLikeFilterRepository extends JpaRepository<CommentLikeFi
     @Query("UPDATE Comment c SET c.likeCount = c.likeCount - 1 WHERE c.id = :commentId")
     void decrementLikeCount(Long commentId);
     boolean existsByUserIdAndCommentId(String userId, Long commentId);
+    @Query("SELECT CASE WHEN COUNT(clf) > 0 THEN true ELSE false END " +
+            "FROM CommentLikeFilter clf WHERE clf.user.id = :userId AND clf.comment.id = :commentId")
+    boolean isUserLikedComment(@Param("userId") String userId, @Param("commentId") Long commentId);
+
 }
