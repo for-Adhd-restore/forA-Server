27f3cb5b73bb8a362cec6a9ed2aa2014367dc2d3
newoceanwave <leeseohyun02@naver.com>
2025-03-28 13:49:14 +0900

    feat: 알림 컨트롤러 내 테스트 api 추가

--- src/main/java/com/project/foradhd/domain/board/web/controller/NotificationController.java
+++ src/main/java/com/project/foradhd/domain/board/web/controller/NotificationController.java
@@ -1,9 +1,12 @@
 package com.project.foradhd.domain.board.web.controller;
 
 import com.project.foradhd.domain.board.business.service.NotificationService;
+import com.project.foradhd.domain.board.business.service.PostService;
+import com.project.foradhd.domain.board.persistence.enums.Category;
 import com.project.foradhd.global.AuthUserId;
 import com.project.foradhd.global.util.SseEmitters;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -15,6 +18,7 @@ public class NotificationController {
 
     private final NotificationService notificationService;
     private final SseEmitters sseEmitters;
+    private final PostService postService;
 
     @GetMapping("/sse")
     public SseEmitter streamSseMvc(@AuthUserId String userId) {
@@ -52,5 +56,23 @@ public class NotificationController {
         notificationService.createNotification(userId, message);
         sseEmitters.sendNotification(userId, message);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/top-posts")
+    public ResponseEntity<?> testTopPosts(@RequestParam(required = false) String category) {
+        if (category == null) {
+            postService.getTopPosts(PageRequest.of(0, 10)); // 알림 발생
+        } else {
+            postService.getTopPostsByCategory(Category.valueOf(category), PageRequest.of(0, 10));
+        }
+        return ResponseEntity.ok("알림 테스트 완료 (TOP10)");
+    }
+
+    @PostMapping("/comment")
+    public ResponseEntity<?> testAddComment(@RequestParam Long postId,
+                                            @RequestParam String content,
+                                            @RequestParam String userId) {
+        postService.addComment(postId, content, userId);
+        return ResponseEntity.ok("댓글 알림 테스트 완료");
     }
 }
