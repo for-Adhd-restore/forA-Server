f778c2b68caf87ac8a4298cd32fb727625e8a92b
kingjinyong <wlsdydwhswkf@gmail.com>
2025-05-27 20:03:25 +0900

    bugfix: phoneRegex 병원 전화번호 정규표현식 수정
    - 050713336072 와 같은 가상 번호로 등록 되어있는 병원 조회 시 정규표현식에 있지 않아 병원 조회가 되지 않는 오류가 있었습니다.
    - 이에 정규 표현식을 수정하여 오류를 수정하였습니다.

--- src/main/java/com/project/foradhd/global/serializer/PhoneSerializer.java
+++ src/main/java/com/project/foradhd/global/serializer/PhoneSerializer.java
@@ -5,24 +5,26 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.project.foradhd.global.exception.InternalSystemException;
-
 import java.io.IOException;
 
 public class PhoneSerializer extends JsonSerializer<String> {
 
-    private static final String phoneRegex = "^(010|02|031|032|033|041|042|043|044|051|052|053|054|055|061|062|063|064)(\\d{3,4})(\\d{4})$";
+    private static final String phoneRegex = "^(\\d{2,4})(\\d{3,4})(\\d{4})$";
     private static final String phoneFormat = "$1-$2-$3";
 
     @Override
-    public void serialize(String value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(String value, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+            throws IOException {
         // value가 null이거나 빈 문자열이면 null로 출력
         if (value == null || value.trim().isEmpty()) {
             jsonGenerator.writeNull();
             return;
         }
 
-        if (!value.matches(phoneRegex)) throw new InternalSystemException(
-                new JsonParseException("Phone Number Format does not match the regex: " + phoneRegex));
+        if (!value.matches(phoneRegex)) {
+            throw new InternalSystemException(
+                    new JsonParseException("Phone Number Format does not match the regex: " + phoneRegex));
+        }
         jsonGenerator.writeString(value.replaceFirst(phoneRegex, phoneFormat));
     }
 }
